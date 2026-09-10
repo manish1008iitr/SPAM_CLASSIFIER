2f4103b3855b219a5959b78dd5058e0585b22f4b
manish1008iitr <m.k.dangi99@gmail.com>
2026-09-10 12:22:46 -0700

    Made data ingestion and saved test and train file in data directory

---
+++ src/data_ingestion.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import os 
+import yaml 
+from sklearn.model_selection import train_test_split
+
+def load_params(params_path:str) -> dict:
+    with open(params_path, 'r') as file:
+        params = yaml.safe_load(file)
+    return params
+
+def data_loader(path:str) -> pd.DataFrame:
+    df = pd.read_csv(path)
+    return df 
+
+def data_processor(df:pd.DataFrame) -> pd.DataFrame:
+    df.drop(columns = ['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], inplace = True)
+    df.rename(columns = {
+        'v1': 'target', 
+        'v2': 'text'
+        }, inplace = True)
+    return df
+
+def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path:str) -> None:
+    raw_data_path = os.path.join(data_path,"_raw")
+    os.makedirs(raw_data_path, exist_ok=True)
+    train_data.to_csv(os.path.join(raw_data_path,"train.csv"))
+    test_data.to_csv(os.path.join(raw_data_path,"test.csv"))
+
+def main():
+    params = load_params(r"C:\Users\Manish kumar\OneDrive\Documentos\SPAM_CLASSIFIER\params.yaml")
+    test_size = params["data_ingestion"]["test_size"]
+    data = data_loader(r"C:\Users\Manish kumar\OneDrive\Documentos\SPAM_CLASSIFIER\experiments\spam.csv") 
+    processed_data = data_processor(data)
+
+    train_data, test_data = train_test_split(
+        processed_data, test_size = test_size, random_state = 42
+    )
+
+    save_data(train_data, test_data, data_path="./data")
+
+if __name__ == "__main__":
+    main()
+        
